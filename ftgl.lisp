4b7dfd8455f18d6eed1e4339a751cb3d6d1f9696
Julian Squires <julian@cipht.net>
2008-10-28 16:21:50 -0400

    Slightly nicer WITH-FONT macro.

--- ftgl.lisp
+++ ftgl.lisp
@@ -2,6 +2,13 @@
 
 (in-package :cl-ftgl)
 
-(defmacro with-font ((var type file) &body body)
+(defmacro with-font ((var type file &key (size 12 size-p) (res 72) (char-map :unicode char-map-p)) &body body)
+  "Wraps BODY in CREATE-type-FONT and DESTROY-FONT, optionally setting
+some of the font parameters per keywords."
   `(let ((,var (,(intern (format nil "CREATE-~A-FONT" type) (find-package :cl-ftgl)) ,file)))
-     (unwind-protect (progn ,@body) (destroy-font ,var))))
+     (unwind-protect
+          (progn
+            ,(when size-p `(ftgl:set-font-face-size ,var ,size ,res))
+            ,(when char-map-p `(ftgl:set-font-char-map ,var ,char-map))
+            ,@body)
+       (destroy-font ,var))))
