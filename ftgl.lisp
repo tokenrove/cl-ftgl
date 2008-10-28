c299475919e388ec55e3e40d5cea42085536d65f
Julian Squires <julian@cipht.net>
2008-10-28 15:55:16 -0400

    Fix WITH-FONT.

--- ftgl.lisp
+++ ftgl.lisp
@@ -3,5 +3,5 @@
 (in-package :cl-ftgl)
 
 (defmacro with-font ((var type file) &body body)
-  `(let ((,var (,(intern (format nil "CREATE-~A-FONT" type)) ,file)))
+  `(let ((,var (,(intern (format nil "CREATE-~A-FONT" type) (find-package :cl-ftgl)) ,file)))
      (unwind-protect (progn ,@body) (destroy-font ,var))))
